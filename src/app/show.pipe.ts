62ee7fc118d9c3dabd7dcbc430ccfb7ac9862e4c
Robbie Kruszynski <robbiekruszynski@gmail.com>
2019-04-05 08:43:05 -0700

    add routing / api key info

--- src/app/show.pipe.ts
+++ src/app/show.pipe.ts
@@ -1,12 +1,12 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {Data} from './data/data.model';
-
-@Pipe({
-
-})
-
-export class ShowPipe implements PipeTransform {
-  transform(input: Data[], args) {
-    return input;
-  }
-}
+// import {Pipe, PipeTransform} from '@angular/core';
+// import {Data} from './data/data.model';
+//
+// @Pipe({
+//
+// })
+//
+// export class ShowPipe implements PipeTransform {
+//   transform(input: Data[], args) {
+//     return input;
+//   }
+// }
